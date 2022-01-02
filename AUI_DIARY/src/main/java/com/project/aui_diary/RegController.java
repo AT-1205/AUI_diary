19eeb43db9d06657be9d4ce339fa5782cbc1e5d0
AT-1205 <73571496+AT-1205@users.noreply.github.com>
2022-01-02 00:19:33 -1200

    Add files via upload

---
+++ AUI_DIARY/src/main/java/com/project/aui_diary/RegController.java
@@ -0,0 +1,15 @@
+package com.project.aui_diary;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class RegController {
+    public void showContacts(MouseEvent mouseEvent) {
+    }
+
+    public void exit(MouseEvent mouseEvent) {
+    }
+
+    public void goBack(ActionEvent actionEvent) {
+    }
+}
